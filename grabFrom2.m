8b7fb69456c0535cbf74ee3a7bcc525037576834
lydzbinski <brandon.lydzbinski@uqconnect.edu.au>
2014-11-11 21:56:45 +1000

    Create grabFrom2.m

---
+++ grabFrom2.m
@@ -0,0 +1,25 @@
+function [ok] = grabFrom2(a)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+%% Grabs Cup
+setSpeed(0.28,0.28,0.4);
+setMotorAngles(a, 105, 100);
+% pause(1)
+setSpeed(0.28,0.28,0.28);
+setMotorAngles(a, 150, 130);
+% setSpeed(0.1,0.1,0.1);
+setMotorAngles(a, 180,150);
+while (1),
+    temp = readMotorAngles();
+    tol = 180/10; %30
+    if(~((180 - tol) > temp(2)) || ((180 + tol) < temp(2))),
+       break 
+    end
+end
+
+ok = 1;
+
+end
+
